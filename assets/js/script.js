3d24129d7ff5ff6fa39e4c26dc5296d7be8d32e4
eldiar36 <eldiarrd@gnail.com>
2021-04-03 11:24:41 +0600

    edit1

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,58 +14,122 @@ $('.popular-slider').slick({
         },
         {
             breakpoint: 767,
-            settings:{
+            settings: {
                 slidesToShow: 1.15,
             }
         },
     ]
 });
+
 function burgerMenu(selector) {
     let menu = $(selector);
     let button = menu.find(".burger-menu_button");
     let links = menu.find(".link");
     let overlay = menu.find("burger-menu_overlay");
-    button.on("click",(e) => {
+    button.on("click", (e) => {
         e.preventDefault();
         toogleMenu();
     });
     links.on("click", () => toogleMenu());
     overlay.on("click", () => toogleMenu());
+
     function toogleMenu() {
         menu.toggleClass('burger-menu_active');
 
         if (menu.hasClass('burger-menu_active')) {
-            $('body').css("overflow","hidden");
-        }else  {
-            $("body").css("overflow","visible")
+            $('body').css("overflow", "hidden");
+        } else {
+            $("body").css("overflow", "visible")
         }
     }
 }
+
 burgerMenu('.burger-menu');
 window.onscroll = function showHeader() {
-    let header =    document.querySelector(".header");
+    let header = document.querySelector(".header");
     if (window.pageYOffset > 200) {
         header.classList.add("header-fixed")
-    }
-    else  {
+    } else {
         header.classList.remove("header-fixed")
     }
 };
 let bg = document.querySelector('.banner-parallax');
-window.addEventListener('mousemove', function(e) {
+window.addEventListener('mousemove', function (e) {
     let x = e.clientX / window.innerWidth;
     let y = e.clientY / window.innerHeight;
     bg.style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)';
 });
 let bgMobile = document.querySelector('.banner-parallax');
-window.addEventListener('scroll', function(e) {
+window.addEventListener('scroll', function (e) {
     let x = e.clientX / window.innerWidth;
     let y = e.clientY / window.innerHeight;
     bg.style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)';
 });
 let bgSecond = document.querySelector('.banner-parallax-two');
-window.addEventListener('mousemove', function(e) {
+window.addEventListener('mousemove', function (e) {
     let x = e.clientX / window.innerWidth;
     let y = e.clientY / window.innerHeight;
     bg.style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)';
 });
+jQuery(($) => {
+    $('.select').on('click', '.select__head', function () {
+        if ($(this).hasClass('open')) {
+            $(this).removeClass('open');
+            $(this).next().fadeOut();
+        } else {
+            $('.select__head').removeClass('open');
+            $('.select__list').fadeOut();
+            $(this).addClass('open');
+            $(this).next().fadeIn();
+        }
+    });
+
+    $('.select').on('click', '.select__item', function () {
+        $('.select__head').removeClass('open');
+        $(this).parent().fadeOut();
+        $(this).parent().prev().text($(this).text());
+        $(this).parent().prev().prev().val($(this).text());
+    });
+
+    $(document).click(function (e) {
+        if (!$(e.target).closest('.select').length) {
+            $('.select__head').removeClass('open');
+            $('.select__list').fadeOut();
+        }
+    });
+});
+// Убавляем кол-во по клику
+$('.quantity_inner .bt_minus').click(function () {
+    let $input = $(this).parent().find('.quantity');
+    let count = parseInt($input.val()) - 1;
+    count = count < 1 ? 1 : count;
+    $input.val(count);
+});
+// Прибавляем кол-во по клику
+$('.quantity_inner .bt_plus').click(function () {
+    let $input = $(this).parent().find('.quantity');
+    let count = parseInt($input.val()) + 1;
+    count = count > parseInt($input.data('max-count')) ? parseInt($input.data('max-count')) : count;
+    $input.val(parseInt(count));
+});
+// Убираем все лишнее и невозможное при изменении поля
+$('.quantity_inner .quantity').bind("change keyup input click", function () {
+    if (this.value.match(/[^0-9]/g)) {
+        this.value = this.value.replace(/[^0-9]/g, '');
+    }
+    if (this.value == "") {
+        this.value = 1;
+    }
+    if (this.value > parseInt($(this).data('max-count'))) {
+        this.value = parseInt($(this).data('max-count'));
+    }
+});
+jQuery(document).ready(function ($) {
+    $('.booking-popup').magnificPopup({
+        type: 'inline',
+        fixedContentPos: true,
+
+    });
+});
+
+
